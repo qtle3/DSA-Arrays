b1838bac67df642b81397ecb9ca9518818ba589e
Quocviet Le <quocvietlet@gmail.com>
2024-09-17 00:25:12 -0500

    solved questions #4 & #5

--- Array Exercise #2.py
+++ Array Exercise #2.py
@@ -1,4 +1,4 @@
-# You have a list of your favorite marvel super heros.
+# You have a list of your favorite marvel super heroes.
 # spider man, thor, hulk, iron man, captain america
 # Using this find out,
 
@@ -8,20 +8,28 @@
 # 4. Now you don't like thor and hulk because they get angry easily :)
 #    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
 #    Do that with one line of code.
-# 5. Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
+# 5. Sort the heroes list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
 
 # Solution:
-heros = ["spider man", "thor", "hulk", "iron man", "captain america"]
+heroes = ["spider man", "thor", "hulk", "iron man", "captain america"]
 
 # answer #1:
-answer_1 = len(heros)
+answer_1 = len(heroes)
 print("length of list:", answer_1)
 
 # answer #2:
-heros.append("black panther")
-print("Add 'black panther' at the end of this list", heros)
+heroes.append("black panther")
+print("Add 'black panther' at the end of this list:", heroes)
 
 # answer #3:
-heros.remove("black panther")
-heros.insert(3, "black panther")
-print("remove black panther and add 'black panther' after 'hulk'", heros)
+heroes.remove("black panther")
+heroes.insert(3, "black panther")
+print("remove black panther and add 'black panther' after 'hulk':", heroes)
+
+# answer #4:
+heroes[1:3] = ["dr strange"]
+print("remove thor and hulk from list and replace them with doctor strange:", heroes)
+
+# answer #5:
+heroes.sort()
+print("Sort the heroes list in alphabetical order:", heroes)
